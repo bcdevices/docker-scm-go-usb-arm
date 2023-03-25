ed8faa114718167356462a090e3ffb4ca4a1a408
Ivo Clarysse <ivo@bcdevices.com>
2023-03-25 12:43:34 +0100

    singlestep

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -2,4 +2,8 @@
 
 git config --global --add safe.directory "$PWD"
 
-VERBOSE=y make distclean dist test
+VERBOSE=y make distclean test
+TARGET_ARCH=linux-arm64 VERBOSE=y make
+TARGET_ARCH=linux-armhf VERBOSE=y make
+TARGET_ARCH=linux-amd64 VERBOSE=y make
+VERBOSE=y make dist-archive
